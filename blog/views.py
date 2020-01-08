2099b94cded994949102434e27d0c550940d5b7b
GhitaAlexandruIon <ghita.alexandru6@gmail.com>
2020-01-08 22:44:54 +0200

    Added views for buttons:drafts,publish, delete post and view for unpublished post page

--- blog/views.py
+++ blog/views.py
@@ -19,7 +19,7 @@ def post_detail(request, pk):
 
 
 def post_new(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
@@ -27,9 +27,9 @@ def post_new(request):
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
-        else:
-            form = PostForm()
-            return render(request, 'blog/post_edit.html', {'form': form})
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 
 def post_edit(request, pk):
@@ -45,3 +45,20 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
